d6f4dfcdacf30820b37f899aabe6d9dad91030b9
delimaalves.gustavo <delimaalves.gustavo@gmail.com>
2020-06-15 21:17:47 -0300

    Add project files.

---
+++ Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAulaObj
+{
+    public class Produto
+    {
+        public string Nome;
+
+        public double Preco;
+
+        public int Qtd;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleAulaObj
+{
+    class Program
+    {
+        //produto 
+        static void Main(string[] args)
+        {
+            // Produto - Nome, preco, qtd  
+            //Produto produtoA, produtoB;
+            //produtoB = new Produto();
+
+            //List<Produto> produtos = new List<Produto>();
+
+
+            //produtos.Add(produtoB);
+
+            //for (int i = 0; i <= 2; i++)
+            //{
+            //    produtoA = new Produto();
+
+            //    Console.WriteLine("Nome do Produto:");
+            //    produtoA.Nome = Console.ReadLine();
+            //    Console.WriteLine("Preço do produto:");
+            //    produtoA.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //    Console.WriteLine("Quantidade:");
+            //    produtoA.Qtd = int.Parse(Console.ReadLine());
+
+            //    produtos.Add(produtoA);
+            //}
+
+            //foreach (var item in produtos)
+            //{
+            //    Console.WriteLine("Temos " + item.Qtd + " " + item.Nome + " no valor de R$" + item.Preco);
+            //}
+
+
+
+        }
+    }
+}
