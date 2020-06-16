4c550fe2408eb89ffe43af9d63cb47b887e0eb2b
delimaalves.gustavo <delimaalves.gustavo@gmail.com>
2020-06-15 22:20:42 -0300

    Programa Produto e Triangulo

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleAulaObj
 {
     class Program
     {
         //produto 
+
         static void Main(string[] args)
         {
-            // Produto - Nome, preco, qtd  
+
+            //PRODUTO
+
+
+            //Produto - Nome, preco, qtd
+
             //Produto produtoA, produtoB;
             //produtoB = new Produto();
 
@@ -37,6 +44,51 @@ namespace ConsoleAulaObj
             //    Console.WriteLine("Temos " + item.Qtd + " " + item.Nome + " no valor de R$" + item.Preco);
             //}
 
+
+
+
+            // TRIANGULO
+
+
+            //Triangulo x, y;
+
+            //x = new Triangulo();
+            //y = new Triangulo();
+
+            //Console.WriteLine("Medidas do triângulo X:");
+
+            //x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.WriteLine("Medidas do triângulo Y:");
+            //y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //double p = (x.A + x.B + x.C) / 2.0;
+
+            //double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            //p = (y.A + y.B + y.C) / 2.0;
+
+            //double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+
+            //Console.WriteLine("Área de X =  " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("Área de Y =  " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+            //if (areaX > areaY)
+            //{
+            //    Console.WriteLine("Área de X é a maior");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Área de Y é a maior");
+
+            //}
 
         }
     }
---
+++ Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAulaObj
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+
+    }
+}
